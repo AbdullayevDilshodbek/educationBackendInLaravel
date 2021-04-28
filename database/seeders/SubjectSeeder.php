e2e4df4c24f26066a668e39691404bd98ba6b477
Abdullayev Dilshodbek <dondilshod096@gmail.com>
2021-04-28 17:08:01 +0500

    test

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -33,6 +33,11 @@ class SubjectSeeder extends Seeder
                 ],
                 [
                     'subject_name' => 'Bialogiya'
+                ],
+                [
+                    'google' => 'Bialogiya',
+                    'toga' => 'Bialogiya',
+                    'boga' => 'Bialogiya',
                 ]
             ]);
     }
