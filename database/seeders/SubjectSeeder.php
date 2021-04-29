b16c0c0940539c2b0e8a372c58bafd6f529b570b
Abdullayev Dilshodbek <dondilshod096@gmail.com>
2021-04-29 11:16:48 +0500

    add new subject

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -33,6 +33,9 @@ class SubjectSeeder extends Seeder
                 ],
                 [
                     'subject_name' => 'Bialogiya'
+                ],
+                [
+                    'subject_name' => 'Tarix'
                 ]
             ]);
     }
