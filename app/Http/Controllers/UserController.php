5c9cee389b4485890cc722af9d54df6681c15dfe
AbdullayevDilshodbek <dondilshod096@gmail.com>
2022-09-28 00:07:21 +0500

    add reload composer to CI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use App\Repositories\User\UserInterface;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
 class UserController extends Controller implements UserInterface
 {
     private UserInterface $userRepository;
